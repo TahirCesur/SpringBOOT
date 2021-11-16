92bf63be9b00e5664ad4a3b8ec616b26f45d5811
Tahir Cesur <delenkar2825@gmail.com>
2021-11-17 01:36:45 +0300

    F01

---
+++ src/day01_MultiThreading/MultiThreading04.java
@@ -0,0 +1,51 @@
+package day01_MultiThreading;
+
+public class MultiThreading04 {
+
+    public static void main(String[] args){
+        Brackets2 obj3 = new Brackets2();
+        Brackets2 obj4 = new Brackets2();
+        Thread thread1 = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                for(int i=1; i<=7; i++) {
+                    obj3.generateBrackets();
+                }
+            }
+        });
+        thread1.start();
+
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for(int i=1; i<=7; i++){
+                    obj4.generateBrackets();
+                }
+            }
+        });
+        thread2.start();
+    }
+}
+
+class Brackets2 {
+    public void generateBrackets() {
+        synchronized (this) {
+            for (int i = 1; i <= 10; i++) {
+                if (i <= 5) {
+                    System.out.print("[");
+                } else {
+                    System.out.print("]");
+                }
+            }
+        }
+        System.out.println();
+        for(int i=1; i<=10; i++) {
+            try {
+                Thread.sleep(25);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ src/day02_Volatile/C01_Volatile.java
@@ -0,0 +1,36 @@
+package day02_Volatile;
+
+// Volatile durumunda thread'in objeye bağımlılığı söz konusudur.
+public class C01_Volatile {
+
+    volatile public static int flag = 0;
+    public static void main(String[] args) {
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    if (flag == 0) {
+                        System.out.println("thread1 calisir durumda...");
+                    } else {
+                        break;
+                    }
+                }
+            }
+        });
+        thread1.start();
+
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                flag = 1;
+                System.out.println("flag variable inin degeri guncellendi");
+            }
+        });
+        thread2.start();
+    }
+}
